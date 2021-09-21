b9aa797d90491f2faabd2d9f478f8f32f9b06944
not-pope <72689979+not-pope@users.noreply.github.com>
2021-09-21 15:25:12 +0200

    more testing

--- counter/script.js
+++ counter/script.js
@@ -1,6 +1,18 @@
+// Given that I don't know the differance between two 
+// options I have (or better yet two implementations),
+// I will keep both and comment one out.
+
+// //---Option I---
+// const output = document.getElementById("output");
+
+// let countClicks = () => {
+//     let clicks = parseInt(output.innerText) + 1;
+//     output.innerText=clicks;
+// }
+
+// ---Option II---
 const output = document.getElementById("output");
 let clicks = parseInt(output.innerText);
-
 //Im not sure, but I think that using one variable to
 //keep clicks would be faster(but prop not enough) and
 //yet it will not make and differance
@@ -8,6 +20,5 @@ let clicks = parseInt(output.innerText);
 //let clicks = parseInt(output.innerText); global var
 //output.innerText=clicks++;
 let countClicks = () => {
-    // let clicks = parseInt(output.innerText) + 1;
     output.innerText=++clicks;
 }
