8d95296ec0c6055e41fa6cf1016795ea9a9679ca
Luka Ivanovic <ivanovic@tutanota.de>
2021-09-22 01:54:42 +0200

    done with temperature converter

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # 7guis
+
+7 simple guis in plain html+css+js, inspired by https://7guis.bradwoods.io/
+
+###guis:
+1.counter
+2.temperature converter
+3.
+4.
+5.
+6.
+7._
--- temperature-converter/script.js
+++ temperature-converter/script.js
@@ -1,28 +1,32 @@
-// Given that I don't know the differance between two 
-// options I have (or better yet two implementations),
-// I will keep both and comment one out.
+// you could create list (or object/dict) of these elems and somehow you don't
+// even need to reference that obj when accessin' them. like so
+// const elems = {
+//   celsius: document.querySelector("input#celsius"),
+//   fahrenheit: document.querySelector("input#fahrenheit"),
+// };
+// i though i need to use elems.celsius but sometimes you dont (apparently)
+// i though about wrting second version but its more or less the same thing
+// also, these two are the same:
+// const {value} = event.target;
+// const value = event.target.value;
+// also oninput you can create fn that passes an event, and you can access
+// event.target to see who called it and do some smart shit with it that way
 
-//---Option I---
-const output = document.getElementById("output");
+let celsius = document.getElementById("celsius");
+let fahrenheit = document.getElementById("fahrenheit");
 
-let countClicks = () => {
-    let clicks = parseInt(output.innerText) + 1;
-    //also, I dont think there is much of a diff in 
-    //using const over let here
-    output.innerText=clicks;
+
+const celsToFahr = (temp) => {
+    return (temp * 1.8 + 32);
+}
+const fahrToCels = (temp) => {
+    return ((temp-32) * 0.5555555555555556);
 }
 
-// ---Option II---
-//Im not sure, but I think that using one variable to
-//keep clicks would be faster(but prop not enough) and
-//yet it will not make any functional differance
-// const output = document.getElementById("output");
-// let clicks = parseInt(output.innerText);
-// let countClicks = () => {
-//     output.innerText=++clicks;
-// }
+const cels = () => {
+    fahrenheit.value=celsToFahr(celsius.value).toFixed(2);
+}
 
-//one additional thing, there is more ways to select
-//output element
-//document.getElementById('output');
-//doucment.querySelector('#output');
+const fahr = () => {
+    celsius.value=fahrToCels(fahrenheit.value).toFixed(2);
+}
