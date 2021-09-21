abc335e264fd9f4ba468d70ae5d69f4a4c8e7455
not-pope <72689979+not-pope@users.noreply.github.com>
2021-09-21 19:40:21 +0200

    slowly adding style

--- counter/script.js
+++ counter/script.js
@@ -2,23 +2,27 @@
 // options I have (or better yet two implementations),
 // I will keep both and comment one out.
 
-// //---Option I---
-// const output = document.getElementById("output");
+//---Option I---
+const output = document.getElementById("output");
 
-// let countClicks = () => {
-//     let clicks = parseInt(output.innerText) + 1;
-//     output.innerText=clicks;
-// }
+let countClicks = () => {
+    let clicks = parseInt(output.innerText) + 1;
+    //also, I dont think there is much of a diff in 
+    //using const over let here
+    output.innerText=clicks;
+}
 
 // ---Option II---
-const output = document.getElementById("output");
-let clicks = parseInt(output.innerText);
 //Im not sure, but I think that using one variable to
 //keep clicks would be faster(but prop not enough) and
-//yet it will not make and differance
-//like:
-//let clicks = parseInt(output.innerText); global var
-//output.innerText=clicks++;
-let countClicks = () => {
-    output.innerText=++clicks;
-}
+//yet it will not make any functional differance
+// const output = document.getElementById("output");
+// let clicks = parseInt(output.innerText);
+// let countClicks = () => {
+//     output.innerText=++clicks;
+// }
+
+//one additional thing, there is more ways to select
+//output element
+//document.getElementById('output');
+//doucment.querySelector('#output');
